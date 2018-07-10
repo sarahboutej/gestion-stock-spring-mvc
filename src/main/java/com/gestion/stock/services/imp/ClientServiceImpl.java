7d0e3fb29fff5ed5a66e83aa66b3d35f14569a8e
GeeketteMind <boutej.sarra@gmail.com>
2018-07-10 16:52:35 +0100

    Message de validation

---
+++ src/main/java/com/gestion/stock/services/imp/ClientServiceImpl.java
@@ -0,0 +1,64 @@
+package com.gestion.stock.services.imp;
+
+import java.util.List;
+
+import com.gestion.stock.entities.Client;
+import com.gestion.stock.services.IClientService;
+
+public class ClientServiceImpl implements IClientService{
+
+	@Override
+	public Client save(Client entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client update(Client entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> selectAll(String sortField, String sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Client getById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client findOne(String paramName, Object paramValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client findOne(String[] paramNames, Object[] paramValues) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int findCountBy(String paramName, String paramValue) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
